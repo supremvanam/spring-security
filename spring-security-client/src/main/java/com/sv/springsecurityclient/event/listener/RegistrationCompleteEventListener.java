d60afaf6bc107b8381f6acb11b4d1826ad15be39
Suprem Vanam <supremvanam.gsp@gmail.com>
2022-09-06 17:01:28 -0400

    Verification Token for User Registration

--- spring-security-client/src/main/java/com/sv/springsecurityclient/event/listener/RegistrationCompleteEventListener.java
+++ spring-security-client/src/main/java/com/sv/springsecurityclient/event/listener/RegistrationCompleteEventListener.java
@@ -1,12 +1,20 @@
 package com.sv.springsecurityclient.entity.listener;
 
+import com.sv.springsecurityclient.entity.User;
 import com.sv.springsecurityclient.event.RegistrationCompleteEvent;
 import org.springframework.context.ApplicationListener;
+
+import java.util.UUID;
 
 public class RegistrationCompleteEventListener implements ApplicationListener<RegistrationCompleteEvent> {
 
     @Override
     public void onApplicationEvent(RegistrationCompleteEvent event) {
         // create the verification token for the user
+        User user = event.getUser();
+        String token = UUID.randomUUID().toString();
+        
+
+        // send mail to user
     }
 }
