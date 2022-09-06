2865338e4f3d00e6f18ccb82607982dbcc536865
Suprem Vanam <supremvanam.gsp@gmail.com>
2022-09-06 17:04:07 -0400

    Save verification token from user service

--- spring-security-client/src/main/java/com/sv/springsecurityclient/event/listener/RegistrationCompleteEventListener.java
+++ spring-security-client/src/main/java/com/sv/springsecurityclient/event/listener/RegistrationCompleteEventListener.java
@@ -1,19 +1,24 @@
-package com.sv.springsecurityclient.entity.listener;
+package com.sv.springsecurityclient.event.listener;
 
 import com.sv.springsecurityclient.entity.User;
 import com.sv.springsecurityclient.event.RegistrationCompleteEvent;
+import com.sv.springsecurityclient.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 
 import java.util.UUID;
 
 public class RegistrationCompleteEventListener implements ApplicationListener<RegistrationCompleteEvent> {
 
+    @Autowired
+    private UserService userService;
     @Override
     public void onApplicationEvent(RegistrationCompleteEvent event) {
         // create the verification token for the user
         User user = event.getUser();
         String token = UUID.randomUUID().toString();
-        
+
+        userService.saveVerificationTokenForUser(token, user);
 
         // send mail to user
     }
