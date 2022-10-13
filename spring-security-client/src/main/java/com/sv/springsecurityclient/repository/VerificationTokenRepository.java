561087d57990241ec579fa35b0c2383a6247607d
Suprem Vanam <supremvanam.gsp@gmail.com>
2022-10-13 12:33:00 -0400

    Verification Token Repo

--- spring-security-client/src/main/java/com/sv/springsecurityclient/repository/VerificationTokenRepository.java
+++ spring-security-client/src/main/java/com/sv/springsecurityclient/repository/VerificationTokenRepository.java
@@ -1,2 +1,7 @@
-package com.sv.springsecurityclient.repository;public interface VerificationTokenRepository {
+package com.sv.springsecurityclient.repository;
+
+import com.sv.springsecurityclient.entity.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
 }
