ded21eb1231fb003f8981b22dfe87ff06bc83343
Suprem Vanam <supremvanam.gsp@gmail.com>
2022-10-13 12:33:28 -0400

    Save verification token for user

--- spring-security-client/src/main/java/com/sv/springsecurityclient/service/UserServiceImpl.java
+++ spring-security-client/src/main/java/com/sv/springsecurityclient/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.sv.springsecurityclient.entity.User;
 import com.sv.springsecurityclient.entity.VerificationToken;
 import com.sv.springsecurityclient.model.UserModel;
 import com.sv.springsecurityclient.repository.UserRepository;
+import com.sv.springsecurityclient.repository.VerificationTokenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -13,12 +14,15 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final VerificationTokenRepository verificationTokenRepository;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, VerificationTokenRepository verificationTokenRepository) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.verificationTokenRepository = verificationTokenRepository;
     }
+
 
     @Override
     public User registerUser(UserModel userModel) {
@@ -38,5 +42,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public void saveVerificationTokenForUser(String token, User user) {
         VerificationToken verificationToken = new VerificationToken(user, token);
+        verificationTokenRepository.save(verificationToken);
     }
 }
